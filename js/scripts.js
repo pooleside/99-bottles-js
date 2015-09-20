48ab5bc995a2840c8c3cb232b66091a9a50db9b7
pooleside <lpoole26@gmail.com>
2015-09-20 13:53:34 -0700

    "two specs with both unit tests passing"

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 var zeroBottlesSong = function() {
   return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.<br/>"
 }
+
+var oneBottleSong = function(staticNumber) {
+  var userNumber= staticNumber;
+  return userNumber + " bottle of beer on the wall, "  +  userNumber + " bottle of beer. Take one down, pass it around no more bottles of beer on the wall.<br/>"
+}
+
+var twoBottlesSong = function(staticNumber) {
+  var userNumber= staticNumber;
+  return userNumber + " bottles of beer on the wall, "  +  userNumber + " bottles of beer. Take one down, pass it around " + (userNumber -1) + " bottle of beer on the wall.<br/>"
+}
--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,15 @@ describe('zeroBottlesSong', function() {
     expect(zeroBottlesSong()).to.contain("No more");
   });
 });
+
+describe('oneBottleSong', function() {
+  it("returns the singular 'bottle' ", function() {
+    expect(oneBottleSong(1)).to.contain("1 bottle");
+  });
+});
+
+describe('twoBottlesSong', function() {
+  it("returns 1 bottle in the final line ", function() {
+    expect(twoBottlesSong(2)).to.contain("1 bottle");
+  });
+});
