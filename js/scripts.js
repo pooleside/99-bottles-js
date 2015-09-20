aec418cb970555039ff39c4dd84f9bbfce2d95f0
pooleside <lpoole26@gmail.com>
2015-09-20 13:16:12 -0700

    "first unit test passing"

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var zeroBottlesSong = function() {
+  return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.<br/>"
+}
